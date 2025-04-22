e8416dc19382ccddaa38bfcca484169ef08ceaae
zarif007 <zarifhuq786@gmail.com>
2025-04-22 23:45:14 +0600

    feat: added sse server

--- src/utils/scrapeLinkedInJobs.js
+++ src/utils/scrapeLinkedInJobs.js
@@ -8,7 +8,16 @@ export const scrapeLinkedInJobs = async ({
 } = {}) => {  
   const browser = await puppeteer.launch({
     headless: 'new',
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', 
+      '--disable-gpu'
+    ],
     defaultViewport: null,
   });
   
