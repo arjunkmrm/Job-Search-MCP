354e7693781dd97b0d007b7497cf4e07b99a26bf
zarif007 <zarifhuq786@gmail.com>
2025-04-27 23:28:26 +0600

    feat: added dockerfile and smithery.yaml

--- src/stdio-server.js
+++ src/stdio-server.js
@@ -1,5 +1,10 @@
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { server } from "./server_logic.js";
 
-const transport = new StdioServerTransport();
-await server.connect(transport);
+try {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+} catch (error) {
+  console.error("Server startup failed:", error);
+  process.exit(1);
+}
